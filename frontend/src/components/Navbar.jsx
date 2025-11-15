57652402c7899a5f4e0a89cac6fd980b7428dcd3
Srinikesh B <srininiku@gmail.com>
2025-11-16 01:30:22 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,6 +35,7 @@ export default function Navbar() {
       </Link>
 
       {/* Desktop Menu */}
+      <a href="/bulk" className="hover:underline">Bulk Predict</a>
       <div className="hidden md:flex items-center gap-6">
         <Link className={isActive("/")} to="/">Home</Link>
         <Link className={isActive("/predict")} to="/predict">Predict</Link>
