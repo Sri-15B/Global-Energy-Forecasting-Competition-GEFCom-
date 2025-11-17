3926dafac042f1e7693ed8b6fd171e8654fe0cfc
Srinikesh B <srininiku@gmail.com>
2025-11-17 20:17:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import BulkPredict from "./pages/BulkPredict";
-<Route path="/bulk" element={<BulkPredict />} />
-
 
 export default function App() {
   const [logged, setLogged] = useState(false);
