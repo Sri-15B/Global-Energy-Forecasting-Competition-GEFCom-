a953a760d1366616c15a343f50fe56fae4e0835f
Srinikesh B <srininiku@gmail.com>
2025-11-18 18:17:27 +0530

    Delete frontend/src directory

--- frontend/src/apo.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const API_URL =
-  "https://global-energy-forecasting-competition.onrender.com";
-
-export const predictLoad = async (features) => {
-  const res = await axios.post(`${API_URL}/predict`, { features });
-  return res.data.predicted_load;
-};
